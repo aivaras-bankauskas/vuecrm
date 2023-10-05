43014c51fb787243c983779c2bad1b3e42898563
aivaras <aivarasbankauskas@gmail.com>
2023-10-05 14:17:52 +0300

    project setup

---
+++ .eslintrc.cjs
@@ -0,0 +1,58 @@
+module.exports = {
+    env: {
+        node: true,
+    },
+    extends: [
+        'plugin:vue/recommended',
+        'plugin:vue/vue3-essential',
+        'eslint:recommended',
+        '@vue/typescript/recommended',
+        'eslint-config-prettier',
+    ],
+    parserOptions: {
+        parser: '@typescript-eslint/parser',
+        requireConfigFile: false,
+        ecmaVersion: 2020
+    },
+    rules: {
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'error',
+        '@typescript-eslint/no-inferrable-types': 'off',
+        'vue/script-setup-uses-vars': 'off',
+        'vue/multi-word-component-names': 'off',
+        'no-plusplus': [
+            'error',
+            {
+                allowForLoopAfterthoughts: true,
+            },
+        ],
+        'no-param-reassign': 0,
+        indent: ['error', 4, { 'SwitchCase': 1 }],
+        'vue/html-indent': [
+            'error',
+            4,
+        ],
+        'vue/script-indent': [
+            'error',
+            4,
+            { 'baseIndent': 1, 'switchCase': 1 }
+        ],
+        'vue/no-multiple-template-root': 0,
+        'semi': [2, 'always'],
+        'no-trailing-spaces': ['error', { 'skipBlankLines': false }],
+        'vue/no-multi-spaces': ['error', { 'ignoreProperties': false }],
+        'object-curly-spacing': ['error', 'always'],
+        'switch-colon-spacing': ['error', { 'after': true, 'before': false }],
+        'quotes': ['error', 'single'],
+        'vue/no-v-for-template-key': 'off'
+    },
+    'overrides': [
+        {
+            'files': ['*.vue'],
+            'rules': {
+                'indent': 'off'
+            }
+        }
+    ]
+};
