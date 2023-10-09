0472626c6793127c31b6684075395ace9bb67814
aivaras <aivarasbankauskas@gmail.com>
2023-10-09 09:31:05 +0300

    Update routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,15 +1,21 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 import { Component } from 'vue';
+import authentication from './authentication';
 import { navigationItems } from '@/views/layout/navigation.json';
 import  NavigationInterface from '@/interfaces/navigation-interface';
 
 const routes: RouteRecordRaw[] = [];
 
+// Redirect to production page
 routes.push({
     path: '/',
     redirect: '/production'
 });
 
+// Add routes from authentication
+routes.push(...authentication);
+
+// Add routes from navigation
 navigationItems.forEach((item: NavigationInterface): void => {
     const { name, vue } = item.menuItem;
     routes.push({
@@ -22,6 +28,7 @@ navigationItems.forEach((item: NavigationInterface): void => {
     });
 });
 
+// Add not found page
 routes.push(
     {
         name: 'not-found',
