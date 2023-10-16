383aee5936a810db22478d22618da7e9113068c8
aivaras <aivarasbankauskas@gmail.com>
2023-10-16 09:13:33 +0300

    add new component

---
+++ src/components/button-components/ButtonLinkWithText.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+    defineProps({
+        text: {
+            type: String,
+            default: ''
+        },
+        linkText: {
+            type: String,
+            default: ''
+        },
+        to: {
+            type: String,
+            default: ''
+        }
+    });
+</script>
+<template>
+    <div>
+        <span class="text-sm text-color">{{ text }}</span>
+        <RouterLink :to="to" class="ml-2 text-sm text-primary hover:text-gray">{{ linkText }}</RouterLink>
+    </div>
+</template>
