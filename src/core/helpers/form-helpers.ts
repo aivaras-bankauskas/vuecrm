10527c49ef563e3447b4ea10e4c908bd92ebdd68
aivaras <aivarasbankauskas@gmail.com>
2023-10-17 17:49:50 +0300

    add  resetForm to form helper

--- src/core/helpers/form-helpers.ts
+++ src/core/helpers/form-helpers.ts
@@ -32,3 +32,9 @@ export const errorMessage = (
     }
     return '';
 };
+
+export const resetForm = (formData: Record<string, string>): void => {
+    for (const field in formData) {
+        formData[field] = '';
+    }
+};
