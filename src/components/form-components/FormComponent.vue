18ee0782c297f96a3b4dcec1d9e3c3b032202d9c
aivaras <aivarasbankauskas@gmail.com>
2023-10-17 15:16:02 +0300

    optimize code in components

--- src/components/form-components/FormComponent.vue
+++ src/components/form-components/FormComponent.vue
@@ -1,12 +1,10 @@
 <script setup lang="ts">
-    import { defineAsyncComponent, reactive, onMounted } from 'vue';
+    import { reactive } from 'vue';
     import type { PropType } from 'vue';
-    import { useRouter } from 'vue-router';
-    import { getFormData, submitFormData } from '@/core/handlers/form-handler';
+    import APIService from '@/core/services/api-service';
+    import InputComponent from '../input-components/InputComponent.vue';
     import InputInterface from '@/core/interfaces/InputInterface';
     import ConfigInterface from '@/core/interfaces/ConfigInterface';
-
-    const InputGroup = defineAsyncComponent(() => import('@/components/input-components/InputGroup.vue'));
 
     const props = defineProps({
         urlId: {
@@ -27,30 +25,72 @@
         }
     });
 
-    const router = useRouter();
-    const formData = reactive({ ...props.data });
-    const validationErrors = reactive(Object.fromEntries(Object.keys(formData).map(key => [key, ''])));
-    let initialFormData: Record<string, unknown> = {};
+    const formData = reactive<Record<string, string>>(props.data as Record<string, string>);
+    const validationRules = reactive(Object.fromEntries(props.inputs.map(input => [input.inputName, input.rules])));
+    const errors = reactive<Record<string, string>>({});
 
-    onMounted(async() => {
-        if (props.urlId) initialFormData = await getFormData(props.urlId, formData, props.config);
-    });
+    const validateField = (field: string, value: string): string => {
+        const rules = validationRules[field].split('|');
+        let errorMessage: string = '';
+
+        for (const rule of rules) {
+            const [ruleName, ruleValue] = rule.split(':');
+
+            if (ruleName === 'required' && !value) {
+                errorMessage = `${field} is required`;
+                break;
+            } else if (ruleName === 'min' && value.length < parseInt(ruleValue)) {
+                errorMessage = `${field} must be at least ${ruleValue} characters`;
+                break;
+            } else if (ruleName === 'alpha' && !/^[a-zA-Z]+$/.test(value)) {
+                errorMessage = `${field} must contain only alphabetic characters`;
+                break;
+            } else if (ruleName === 'email' && !/\S+@\S+\.\S+/.test(value)) {
+                errorMessage = 'Invalid email format';
+                break;
+            }
+        }
+
+        return errorMessage;
+    };
 
     const submitForm = async (): Promise<void> => {
-        await submitFormData(props.urlId, formData, initialFormData, validationErrors, props.config, props.data);
-        if (Object.values(validationErrors).every(error => error === '') && props.config.redirect !== null) {
-            router.push(props.config.redirect);
+        let isValid = true;
+
+        Object.keys(formData).forEach(field => {
+            const fieldErrors = validateField(field, formData[field] as string);
+
+            if (fieldErrors.length > 0) {
+                isValid = false;
+            }
+            errors[field] = fieldErrors;
+        });
+
+        if (isValid) {
+            await APIService.store(props.config.API, formData);
+            resetForm();
+        }
+    };
+
+    const resetForm = (): void => {
+        for (const field in formData) {
+            formData[field] = '';
         }
     };
 </script>
 
 <template>
-    <form @submit.prevent="submitForm">
-        <InputGroup
-            :form-data="formData"
-            :inputs="inputs"
-            :validation-errors="validationErrors"
-        />
+    <form novalidate @submit.prevent="submitForm">
+        <div v-for="(input, index) in inputs" :key="index">
+            <InputComponent
+                :id="input.inputName"
+                v-model="formData[input.inputName]"
+                :input-name="input.inputName"
+                :input-type="input.inputType"
+                :placeholder="input.inputName"
+                :error-message="errors[input.inputName]"
+            />
+        </div>
         <slot name="buttons" />
     </form>
 
--- src/components/input-components/InputComponent.vue
+++ src/components/input-components/InputComponent.vue
@@ -1,7 +1,5 @@
 <script setup lang="ts">
-    import { computed, ref, watch } from 'vue';
-    import { useI18n } from 'vue-i18n';
-    import IconComponent from '../icon-components/IconComponent.vue';
+    import { computed } from 'vue';
 
     const props = defineProps({
         modelValue: {
@@ -16,89 +14,34 @@
             type: String,
             default: 'text'
         },
-        rules: {
+        placeholder: {
             type: String,
             default: ''
         },
-        error: {
+        errorMessage: {
             type: String,
             default: ''
         }
     });
 
-    const { t, te, locale } = useI18n();
     const emit = defineEmits(['update:modelValue']);
-    const displayedError = ref('');
-    const inputType = ref(props.inputType);
-    const isEyeOpen = ref(false);
 
     const inputStyle = computed(() => {
-        return displayedError.value
+        return props.errorMessage !== ''
             ? 'block w-full rounded-md border-0 py-1.5 px-3 text-gray-dark shadow-sm ring-1 ring-inset ring-danger placeholder:text-gray focus:ring-2 focus:ring-inset focus:ring-danger-light sm:text-sm sm:leading-6'
             : 'block w-full rounded-md border-0 py-1.5 px-3 text-gray-dark shadow-sm ring-1 ring-inset ring-primary-light placeholder:text-gray focus:ring-2 focus:ring-inset focus:ring-primary-light sm:text-sm sm:leading-6';
     });
 
-    console.log(props.rules);
-
-
     const updateValue = (event: Event): void => {
         const target = event.target as HTMLInputElement;
         emit('update:modelValue', target.value);
-        if (props.rules.split('|').includes('required') && target.value === '') {
-            displayedError.value = errorMessage(props.inputName, 'required');
-        } else {
-            displayedError.value = '';
-        }
     };
-
-    const getErrorMessage = (): void => {
-        if (props.error) {
-            displayedError.value = errorMessage(props.inputName, props.error);
-        }
-    };
-
-    const errorMessage = (inputName: string, error: string): string => {
-        if (error) {
-            if (!error.includes(':')) {
-                const errorTemplate = t(`errors.${error}`);
-                const formattedErrorMessage = errorTemplate.replace('{{inputName}}', t(`labels.${inputName}`));
-                return formattedErrorMessage;
-            } else {
-                const [errorType, errorValue] = error.split(':');
-                const errorTemplate = t(`errors.${errorType}`);
-                const formattedErrorMessage = errorTemplate.replace('{{inputName}}', t(`labels.${inputName}`));
-                return formattedErrorMessage.replace('{{count}}', errorValue);
-            }
-        }
-        return '';
-    };
-
-    const getPlaceholderAttribute = (): { placeholder: string } => {
-        const getTranslation = (type: string): string => {
-            const key = `${type}.${props.inputName}`;
-            return te(key) ? t(key) : '';
-        };
-
-        const placeholder: string = getTranslation('placeholders');
-        const label: string = getTranslation('labels');
-
-        return { placeholder: placeholder !== '' ? placeholder : label };
-    };
-
-    const toggleEye = (): void => {
-        isEyeOpen.value = !isEyeOpen.value;
-        if (props.inputName === 'password') {
-            inputType.value = !isEyeOpen.value ? 'password' : 'text';
-        }
-    };
-
-    watch([(): string => props.error, (): string => locale.value], getErrorMessage);
 </script>
 
 <template>
     <div>
         <label :for="inputName" class="block text-sm font-medium text-color text-start mb-2">
-            {{ t(`labels.${inputName}`) }}
+            {{ inputName }}
         </label>
         <div class="relative mt-2 rounded-md shadow-sm">
             <input
@@ -107,17 +50,10 @@
                 :name="inputName"
                 :type="inputType"
                 :class="inputStyle"
-                v-bind="getPlaceholderAttribute()"
-                :data-rules="rules"
+                :placeholder="placeholder"
                 @input="updateValue"
             >
-            <div class="absolute inset-y-0 right-1 flex items-center p-1" @click="toggleEye">
-                <div v-if="inputName === 'password'">
-                    <IconComponent v-if="!isEyeOpen" src="src/assets/icons/eye-close.svg" class="h-6 w-auto text-gray-light cursor-pointer" />
-                    <IconComponent v-else src="src/assets/icons/eye-open.svg" class="h-6 w-auto text-gray-light cursor-pointer" />
-                </div>
-            </div>
         </div>
-        <div class="h-2 text-start text-xs text-danger ml-3 mt-1.5">{{ displayedError }}</div>
+        <div class="h-2 text-start text-xs text-danger ml-3 mt-1.5">{{ errorMessage }}</div>
     </div>
 </template>
