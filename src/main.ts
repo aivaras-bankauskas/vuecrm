e8b1e21f4b0f7c47248ed8a7dc0c42397be5d77c
aivaras <aivarasbankauskas@gmail.com>
2023-10-20 15:40:37 +0300

    add axios interceptors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,34 @@ import './assets/css/main.css';
 import App from './App.vue';
 import router from './router';
 import i18n from './i18n';
+import axios from 'axios';
+import AuthService from '@/core/services/auth-service';
+
+axios.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem('userToken');
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+axios.interceptors.response.use(
+    (response) => {
+        return response;
+    },
+    (error) => {
+        if (error.response.status === 401) {
+            AuthService.signOut();
+            router.push({ name: 'sign-in' });
+        }
+        return Promise.reject(error);
+    }
+);
 
 const app = createApp(App);
 
