b950187dea43bc55b1af8a194dca72c89599c61c
aivaras <aivarasbankauskas@gmail.com>
2023-10-23 12:02:03 +0300

    optimize code

--- src/core/services/auth-service.ts
+++ src/core/services/auth-service.ts
@@ -3,7 +3,7 @@ import APIService from '@/core/services/api-service';
 export default class AuthService {
     static async signup(id: number): Promise<void> {
         const token = 'some-very-secret-token';
-        AuthService.storeToken(id, token, '1h');
+        localStorage.setItem('userToken', JSON.stringify({ id, token }));
     }
 
     static async signIn(email: string, password: string): Promise<boolean> {
@@ -12,7 +12,7 @@ export default class AuthService {
 
         if (user) {
             const token = 'some-very-secret-token';
-            AuthService.storeToken(user.id, token, '1h');
+            localStorage.setItem('userToken', JSON.stringify({ id: user.id, token }));
             return true;
         } else {
             return false;
@@ -23,37 +23,7 @@ export default class AuthService {
         localStorage.removeItem('userToken');
     }
 
-    static storeToken(id: number, token: string, expiresIn: string): void {
-        let expires: number;
-
-        switch (expiresIn) {
-            case '10s':
-                expires = new Date().getTime() + 10000;
-                break;
-            case '1m':
-                expires = new Date().getTime() + 60000;
-                break;
-            case '10m':
-                expires = new Date().getTime() + 600000;
-                break;
-            case '1h':
-                expires = new Date().getTime() + 3600000;
-                break;
-            default:
-                throw new Error('Invalid expiration time');
-        }
-
-        const user = { id, token, expires };
-        localStorage.setItem('userToken', JSON.stringify(user));
-    }
-
-    static isTokenExpired(): boolean {
-        const storedUser = localStorage.getItem('userToken');
-        if (storedUser) {
-            const { expires } = JSON.parse(storedUser);
-            const currentTime = new Date().getTime();
-            return currentTime > expires;
-        }
-        return true;
+    static isTokenSet(): boolean {
+        return !!localStorage.getItem('userToken');
     }
 }
--- src/router/route-guards.ts
+++ src/router/route-guards.ts
@@ -2,7 +2,7 @@ import AuthService from '@/core/services/auth-service';
 import { NavigationGuardWithThis } from 'vue-router';
 
 export const requireAuth: NavigationGuardWithThis<undefined> = (to, _from, next) => {
-    if (to.meta.requiresAuth && AuthService.isTokenExpired()) {
+    if (to.meta.requiresAuth && !AuthService.isTokenSet()) {
         next({ name: 'sign-in' });
     } else {
         next();
