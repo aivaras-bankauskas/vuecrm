82753a2ab06c6490f39ec412120b4df351fe5f12
aivaras <aivarasbankauskas@gmail.com>
2023-11-22 13:46:47 +0200

    TableComponent.vue: update endpoint to router link

--- src/components/table-components/TableComponent.vue
+++ src/components/table-components/TableComponent.vue
@@ -32,7 +32,7 @@
                 <div class="flex justify-between">
                     {{ t(`navigation.${title}`) }}
                     <RouterLink
-                        :to="`/${title}/create`"
+                        :to="`${endpoint}/create`"
                         class="rounded-md bg-primary px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                     >
                         {{ t(`labels.${linkText}`) }}
