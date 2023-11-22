3887a9f7d1b426e135b7c63177af5046971fc834
aivaras <aivarasbankauskas@gmail.com>
2023-11-22 13:48:23 +0200

    TeamPage.vue: add team page

--- src/views/pages/team/TeamPage.vue
+++ src/views/pages/team/TeamPage.vue
@@ -1,5 +1,18 @@
+<script setup lang="ts">
+    import { defineAsyncComponent } from 'vue';
+    import data from './team.json';
+
+    const TableComponent = defineAsyncComponent(() => import('@/components/table-components/TableComponent.vue'));
+
+</script>
+
 <template>
     <div>
-        Team Page
+        <TableComponent
+            :table-headers="data.inputs"
+            :title="data.config.name"
+            :endpoint="data.config.API"
+            link-text="addTeamMember"
+        />
     </div>
 </template>
